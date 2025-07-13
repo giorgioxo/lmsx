9b3b5bd81c88b4f3ea6ed1ec188545093cf9f2d1
giorgio.xo <gikhokhonishvili@gmail.com>
2025-07-13 15:03:15 +0400

    cahgned layout router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutsComponent } from './shared/layouts/layouts.component';
 
 @Component({
   selector: 'lmsx-root',
-  imports: [LayoutsComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
