851759492fcb362dcdbd9935229289f71a89e3c5
giorgio.xo <gikhokhonishvili@gmail.com>
2025-08-04 07:48:11 +0400

    feature/ reviewed layout, created routes for its children, started by dashboard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,24 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 import { LayoutsComponent } from './shared/layouts/layouts.component';
 import { RegisterComponent } from './core/auth/register/register.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutsComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./features/dashboard/dashboard.route').then(
+            (m) => m.DASHBOARD_ROUTES,
+          ),
+      },
+    ],
+  },
+
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: LayoutsComponent },
   { path: 'register', component: RegisterComponent },
 ];
---
+++ src/app/features/dashboard/dashboard.route.ts
@@ -0,0 +1,6 @@
+import { DashboardComponent } from './dashboard.component';
+import { Routes } from '@angular/router';
+
+export const DASHBOARD_ROUTES: Routes = [
+  { path: '', component: DashboardComponent },
+];
