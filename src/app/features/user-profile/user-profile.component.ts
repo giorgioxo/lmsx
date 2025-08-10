c11d0f1b8fc629e3a7651c06fea617d7973e3641
giorgio.xo <gikhokhonishvili@gmail.com>
2025-08-10 19:03:43 +0400

    added user

---
+++ src/app/features/user-profile/user-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lmsx-user-profile',
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
+})
+export class UserProfileComponent {
+
+}
