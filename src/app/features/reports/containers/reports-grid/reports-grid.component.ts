dfde0a35d9bf9cbefffe0ed2a0f051d15c492e22
giorgio.xo <gikhokhonishvili@gmail.com>
2025-08-14 19:35:14 +0400

    feature/ added reports grid

---
+++ src/app/features/reports/containers/reports-grid/reports-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lmsx-reports-grid',
+  imports: [],
+  templateUrl: './reports-grid.component.html',
+  styleUrl: './reports-grid.component.scss'
+})
+export class ReportsGridComponent {
+
+}
